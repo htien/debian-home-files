424969acb55edc0ac1510547f65a441a2f4a0783
Tien Nguyen <al1man@hva.online>
2015-11-23 19:53:59 +0700

    Added setup automatically script.

---
+++ setup-home.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -ex
+
+PREV_DIR=$(pwd)
+DIR="$(cd -P "$(dirname $0)" && pwd)"
+
+cp bash_profile $HOME/.bash_profile
+cp bashrc       $HOME/.bashrc
+cp gitconfig    $HOME/.gitconfig
+cp -R ssh/      $HOME/.ssh
+
+chmod -R u=rwX,go= \
+	$HOME/.bash* \
+	$HOME/.gitconfig \
+	$HOME/.ssh/
+
+# Get to previous working directory
+cd $DIR
